c7a0c7761586d3da6e3e985f37ca4153ecb7a17b
Ryan Kidd <itsryankidd@gmail.com>
2026-08-16 17:03:40 -0700

    Export long animation frame API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-export { observe, isSupported } from "./observe.js";
+export { observe, isSupported, isLongAnimationFrameSupported } from "./observe.js";
 export { InteractionTracker, toInteraction } from "./tracker.js";
+export { LoafTracker, toLongAnimationFrame, toScriptAttribution } from "./loaf.js";
 export type {
   EventTimingEntry,
   Interaction,
+  InteractionReport,
+  LongAnimationFrame,
+  LongAnimationFrameEntry,
   ObserveHandle,
   ObserveOptions,
+  ScriptAttribution,
+  ScriptTimingEntry,
 } from "./types.js";
